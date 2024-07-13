d4cbcf83e5346a3711c9478449e9e72f6f3743f8
shreyareddy111 <245121737111@mvsrec.edu.in>
2024-07-13 12:33:45 +0530

    commit changes

---
+++ Swap.java
@@ -0,0 +1,13 @@
+class Swap{
+	public static void main(String args[])
+	{
+		int a=20;
+		int b=10;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
